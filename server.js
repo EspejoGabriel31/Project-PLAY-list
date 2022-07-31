42894c6759364e673b2de6a0cf75102cb0ee6006
unknown <gespejo2144@gmail.com>
2022-07-31 14:06:32 -0700

    added 404 page

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
@@ -6,4 +7,8 @@ app.get('/', (req,res) => {
     res.send('Hello World')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT) //SET PORT TO 3000
