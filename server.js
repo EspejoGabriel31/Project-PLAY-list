7d03bc27292ea6ad689f8f1b942980dbb931460a
AngeloAbat <angeloabat25@gmail.com>
2022-08-01 16:29:47 -0700

    Added views, configure server.js, added dependency

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
+// EXPRESS & DEPENDENCIES
 require('dotenv').config()
 const express = require('express')
-
 const app = express()
 
+
+// EXPRESS SETTINGS
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require( `express-react-views`).createEngine())
+app.use(express.static('public'))
+
+
+// CONTROLLERS & ROUTES
 app.get('/', (req,res) => {
     res.send('Hello World')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('404')
 })
 
-app.listen(process.env.PORT) //SET PORT TO 3000
+//PORT
+app.listen(process.env.PORT)
