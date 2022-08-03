df0da018e7cb3fd2c279b473a06afdba0cffc6e4
AngeloAbat <angeloabat25@gmail.com>
2022-08-02 21:07:58 -0700

    Fixed Server.js & Added dependencies

--- views/default.jsx
+++ views/default.jsx
@@ -1,23 +1,13 @@
 const React = require('react')
+
 function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+                <title>Play-List</title>
                 <link rel="stylesheet" href="../style.css" />
             </head>
             <body>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href='/'>Home</a>
-                        </li>
-                        <li>
-                            <a href='/playlist'>Playlist</a>
-                        </li>
-                    </ul>
-                </nav>
                 {html.children}
             </body>
         </html>
