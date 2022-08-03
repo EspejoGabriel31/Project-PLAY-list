bf785c403866ecbf22e3be187917597d9c07f19d
unknown <gespejo2144@gmail.com>
2022-08-02 23:23:03 -0700

    added css stylings to default. added Title and nav bar

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express')
 const app = express()
 
 app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 app.get('/', (req,res) => {
     // res.send('Hello World')
--- views/default.jsx
+++ views/default.jsx
@@ -3,11 +3,12 @@ function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
+                <title>PLAY-list</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-                <link rel="stylesheet" href="../style.css" />
+                <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
+                <h1>PLAY-list</h1>
                 <nav>
                     <ul>
                         <li>
@@ -18,7 +19,6 @@ function Def (html) {
                         </li>
                     </ul>
                 </nav>
-                {html.children}
             </body>
         </html>
     )
