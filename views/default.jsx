d336581f2c13f0d3ead46e0361f30b53d2750072
allison ruiz <allisonruiz@allisons-MacBook-Pro-2.local>
2022-08-02 21:11:19 -0700

    editing the home page

--- views/default.jsx
+++ views/default.jsx
@@ -1,7 +1,4 @@
 const React = require('react')
-<<<<<<< HEAD
-
-=======
 function Def (html) {
     return (
         <html>
@@ -28,4 +25,3 @@ function Def (html) {
 }
 
 module.exports = Def
->>>>>>> ruiz
---
+++ views/home.jsx
@@ -0,0 +1,16 @@
+const Def = require('react')
+const Def= require('./default')
+ function home() {
+    return (
+        <Def>
+            <main>
+                <h1>PLAY-List</h1>
+                <a href="/places">
+
+                </a>
+            </main>
+        </Def>
+    )
+ }
+
+ module.exports = home
