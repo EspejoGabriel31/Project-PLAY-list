23c796269179dfb6dd6c7f7eb709d87ce57df994
unknown <gespejo2144@gmail.com>
2022-08-05 14:46:15 -0700

    imported elements from MVC version

--- client/src/App.js
+++ client/src/App.js
@@ -16,21 +16,11 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          <p>{!data ? 'Loading...' : data}</p>
+          {!data ? 'Loading...' : data}
         </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+
     </div>
   );
 }
