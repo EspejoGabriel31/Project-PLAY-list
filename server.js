9b705ba29a7238487572db43638fb597526e7dd2
AngeloAbat <angeloabat25@gmail.com>
2022-08-04 18:27:05 -0700

    fixed

--- server.js
+++ server.js
@@ -13,14 +13,11 @@ app.use(express.urlencoded({extended: true}))
 // app.use(methodOverride('_method'))
 
 //Controllers & Routes
-<<<<<<< HEAD
-app.use('/places', require('.//controllers/playlist_controller'))
-
-// app.use('/playlists', require('./controllers/playlist_controller'))
-=======
 app.use('/playlists', require('./controllers/playlist_controller'))
->>>>>>> 72a4bc4c6c304c5c14f4ed658a22c72519064e40
 
+app.get('/aboutUs', (req,res) =>{
+    res.render('aboutUs')
+})
 
 app.get('/', (req,res) => {
     res.render('home')
