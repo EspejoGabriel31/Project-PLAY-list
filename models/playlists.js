8b7fc57412624f0dff68e86deea39dd2d23d7d65
unknown <gespejo2144@gmail.com>
2022-08-06 14:08:29 -0700

    ...

--- models/playlists.js
+++ models/playlists.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 
  const playlistSchema = new mongoose.Schema({
    name: {type: String, required: true},
-   hasGluten: Boolean,
    image: { type: String, default: 'https://as1.ftcdn.net/v2/jpg/01/31/65/76/1000_F_131657622_S5YkNeyUWFuiQfI64SHdQk7myctG8JgQ.jpg' },
    game: {
       type: String
@@ -14,7 +13,5 @@ const mongoose = require('mongoose')
  playlistSchema.methods.newPlaylist = function() {
    return `${this.name}`
  };
- 
-const Playlist = mongoose.model('Playlist', playlistSchema);
 
- module.exports = Playlist
+ module.exports = mongoose.model('Playlist', playlistSchema);
