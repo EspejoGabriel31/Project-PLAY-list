72bad24130bba8071456b3a09c3a95b0b00f4f64
allison ruiz <=>
2022-08-07 11:29:18 -0700

    new page added

--- client/src/components/New.js
+++ client/src/components/New.js
@@ -2,7 +2,20 @@
 export default function New(props) {
     return(
         <div>
-            
+            <main>
+                <h1>Create a New PLAYlist</h1>
+                <form method="POST" action="/playlists">
+                    <div className="form-group">
+                        <label htmlFor="name">Playlist Name</label>
+                        <input className="form-control" id="name" name="name" required />
+                        </div>
+                        <div className="form-group">
+                        <label htmlFor="playlistItem">Add Game</label>
+                        <input className="form-control" id="game" name="game" required />
+                    </div>
+                    <input className="btn btn-primary" type="submit" value="Add Playlist" />
+                </form>
+            </main>
         </div>
     )
 }
