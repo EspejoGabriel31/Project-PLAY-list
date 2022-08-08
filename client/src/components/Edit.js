12797da9e719249410efa6d7af03e88c12d53fa0
allison ruiz <allisonnruizz@gmail.com>
2022-08-08 14:34:39 -0700

    edit page additions

--- client/src/components/Edit.js
+++ client/src/components/Edit.js
@@ -1,11 +1,26 @@
+import { useState } from "react"
+import axios from 'axios'
+
 export default function Edit() {
+
+    const [name, setName] = useState("")
+    const [games, setGames] = useState([])
+
+    let handleSubmit =async(e) => {
+        e.preventDefault();
+        console.log(name, games)
+        axios.post('/edit', {name, games})
+        .then(r => console.log(r.data))
+        .catch(e => console.log(e))
+    }
     return(
+        <div>
         <main>
         <h1>Edit Playlist</h1>
-        <form method="POST" action="">
+        <form onSubmit={handleSubmit}>
             <div className="form-group">
-                <label htmlFor="playlist">Playlist Name</label>
-                <input className="form-control" id="playlist" name="playlist" value=""/>
+                <label htmlFor="name">Playlist Name</label>
+                <input className="form-control" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} required />
                 </div>
                 {/* //cute pic here?
                 <div>
@@ -13,11 +28,12 @@ export default function Edit() {
                 <input type="url" className="form-control" id="pic" name="pic" />
             </div> */}
             <div className="form-group">
-                <label htmlFor="games">Games</label>
-                <input className="form-control" name="game" required/>
+                <label htmlFor="playlistItem">Edit Games</label>
+                <input className="form-control" id="games" name="games" value={games} onChange={(e) => setGames(e.target.value)} required/>
             </div>
-            <input className="btn btn-primary" type="submit" value="Add Playlist" />
+            <input className="btn btn-primary" type="submit" />
         </form>
     </main>
+    </div>
     )
 }
