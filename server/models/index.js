47261a0779d6a1cb8c91e2ae67f19e9278eb9a6a
allison ruiz <allisonnruizz@gmail.com>
2022-08-08 18:47:50 -0700

    workingg mongo db

---
+++ server/models/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+module.exports.Playlist = require('./playlists')
