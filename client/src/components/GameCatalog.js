4ed25de84c79e6cb0306293070c09d8567dc80ca
AngeloAbat <angeloabat25@gmail.com>
2022-08-09 18:38:33 -0700

    Game Cata small changeS

--- client/src/components/GameCatalog.js
+++ client/src/components/GameCatalog.js
@@ -2,13 +2,14 @@
 import { useState, useRef } from 'react';
 
 // Components 
-import Catalog from './SearchCatalog/Catalog';
-import SearchBar from './SearchCatalog/SearchBar';
-import { DataContext, SearchContext } from './SearchCatalog/Context';
+import Catalog from './GameCatalog/Catalog';
+import SearchBar from './GameCatalog/SearchBar';
+import { DataContext, SearchContext } from './GameCatalog/Context';
 
 export default function GameCatalog(){
     let [data, setData] = useState([])
     let searchInput = useRef('')
+    let [checkState, setCheckState] = useState(false)
   
     const rawgAPI = 'https://rawg.io/api/games?key=b54471c4e46549deb7bf64342ab6e282&search='
   
@@ -28,14 +29,23 @@ export default function GameCatalog(){
       fetchData()
     }
 
+  
     return(
         <div>
-            <SearchContext.Provider value={{term:searchInput,handleSearch: handleSearch}}>
-                <SearchBar/>
-            </SearchContext.Provider>
-            <DataContext.Provider value={data}>
-                <Catalog/>
-            </DataContext.Provider>
+          {checkState ? (
+            <button onClick={() => setCheckState(false)}> false</button>
+          ) : (
+            <button onClick={() => setCheckState(true)}> true </button>
+          )}
+          {checkState ? <div>
+          <SearchContext.Provider value={{term:searchInput,handleSearch: handleSearch}}>
+            <SearchBar/>
+          </SearchContext.Provider>
+          <DataContext.Provider value={data}>
+            <Catalog />
+          </DataContext.Provider>
+          </div>
+          : []}
         </div>
     )
 }
